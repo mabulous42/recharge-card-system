9660723e80fa60e93c572c2c86359318b73467f3
Abbas <tundeeey@gmail.com>
2023-03-23 11:33:43 +0100

    I just successfully wrote the fuction that checks for three conditions before printing the recharge code, 1. chexks if the network and amount is selected, 2. checks if the network is selected and 3. checks if the amount is selected

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function generateCard() {
     let display = document.getElementById('generated-card');
-    display.innerText = Math.floor(Math.random() * 1000000000000000);
+    let network = document.getElementById('network');
+    let amount = document.getElementById('amount');
+
+    if (network.value === "empty" && amount.value === "not-selected") {
+        alert("Please Specify your network provider and the amount to recharge amount");
+    } 
+    
+    else if (amount.value === "not-selected") {
+        alert("Please select an amount");
+    } 
+    
+    else if (network.value === "empty") {
+        alert("Please select a network provider");
+    }
+    
+    else {
+        display.innerText = Math.floor(Math.random() * 1000000000000000);
+    }
+    
 }
