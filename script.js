060231569674503322b07cfb0c19d88bd59c5b8b
Abbas <tundeeey@gmail.com>
2023-03-24 22:14:33 +0100

    I added some other conditions to check if the recharge code, the recharge pin are correct and also if the '#' character is included at the end of the input text

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let display = document.getElementById('generated-card');
 let network = document.getElementById('network');
 let amount = document.getElementById('amount');
 let recharge = document.getElementById('loadCardInput');
-let randomNumber =  Math.floor(Math.random() * 1000000000000000);
+let randomNumber = Math.floor(Math.random() * 1000000000000000);
 
 function generateCard() {
 
     if (network.value === "empty" && amount.value === "not-selected") {
-        alert("Please Specify your network provider and the amount to recharge amount");
+        alert("Please Specify your network provider and the recharge amount");
     }
 
     else if (amount.value === "not-selected") {
@@ -24,23 +24,35 @@ function generateCard() {
 }
 
 function loadAirtime() {
-    
-    if (network.value === "mtn" && recharge.value.startsWith("*555*") && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
-        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successful");
+    console.log(recharge.value.startsWith('*555*'));
+    console.log(recharge.value.includes(randomNumber));
+    console.log(recharge.value.endsWith('#'));
+
+    if (recharge.value === '') {
+        alert("Please enter the recharge numbers before you proceed")
     } 
-    else if (network.value === "mtn" && recharge.value.startsWith("*555*") != '*555*' && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
-        alert("invalid recharge pin");
-    }
+    else if (network.value === "mtn" && recharge.value.startsWith("*555*") && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successfully");
+    }      
     else if (network.value === "glo" && recharge.value.startsWith("*123*") && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
-        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successful");
-    }
+        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successfully");
+    }    
     else if (network.value === "airtel" && recharge.value.startsWith("*126*") && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
-        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successful");
+        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successfully");
     }
     else if (network.value === "etisalat" && recharge.value.startsWith("*232*") && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
-        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successful");
+        alert(network.value.toUpperCase() + " #" + amount.value + " Recharged Successfully");
     }
-     else {
+    else if (!(recharge.value.includes(randomNumber))) {
+        alert("Invalid Recharge Pin")
+    }
+    else if ((recharge.value.startsWith() != '*555' || recharge.value.startsWith() != '*123' || recharge.value.startsWith() != '*126' || recharge.value.startsWith() != '*232') && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert("Invalid network recharge code");
+    } 
+    else if (recharge.value.endsWith('#') != '#') {
+        alert("# is missing")
+    }
+    else {
         alert("recharge failed");
     }
 }
